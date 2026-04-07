cd984b27933a95e0e2e4d886737c3e3e8db37e86
aayush <ar0671362@gmail.com>
2026-04-07 12:40:53 +0530

    redis caching and rate-limiting implemented

--- server/src/redis/index.ts
+++ server/src/redis/index.ts
@@ -1,3 +1,3 @@
 import Redis from "ioredis"
 
-export const redisClient = new Redis("rediss://default:gQAAAAAAAW8UAAIncDFmOTQ3NDdhMjI4YTc0ODhlYTIwMzc4N2I2ZjNhNzVkOHAxOTM5NzI@exciting-liger-93972.upstash.io:6379");
+export const redisClient = new Redis(process.env.REDIS_URL as string);
